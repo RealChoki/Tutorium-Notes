b113b90462e73058170f5c02ff2f3127ccc3cc76
RealChoki <123271772+RealChoki@users.noreply.github.com>
2024-04-29 22:49:23 +0200

    Update method name from antwort to whatsTheAnswer in Kontrollstrukturen4.java

--- Kapiel4Kontrollstrukturen/Kontrollstrukturen4.java
+++ Kapiel4Kontrollstrukturen/Kontrollstrukturen4.java
@@ -1,6 +1,6 @@
 public class Kontrollstrukturen4 {
 
-    public static String antwort(int input) {
+    public static String whatsTheAnswer(int input) {
         if (input % 3 == 0 && input % 7 == 0) {
             return "42";
         } else if (input % 3 == 0) {
@@ -14,8 +14,8 @@ public class Kontrollstrukturen4 {
 
     public static void main(String[] args) {
         // Beispielaufruf
-        int eingabe = 21;
-        String ergebnis = antwort(eingabe);
+        int eingabe = 9;
+        String ergebnis = whatsTheAnswer(eingabe);
         System.out.println("Ergebnis für Eingabe " + eingabe + ": " + ergebnis);
     }
 }
